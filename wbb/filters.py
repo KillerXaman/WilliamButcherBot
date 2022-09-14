28d000026d361720ee45e5f2ad074e17fb90d71d
KillerXaman <102473258+KillerXaman@users.noreply.github.com>
2022-09-14 17:47:42 +0530

    Update filters.py

--- wbb/filters.py
+++ wbb/filters.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from typing import List, Union
-from sample_config import COMMAND_PREFIXES
+from sample_config import USERBOT_PREFIXES
 
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
@@ -10,4 +10,4 @@ other_filters2 = (
 
 
 def command(commands: Union[str, List[str]]):
-    return filters.command(commands, COMMAND_PREFIXES)
+    return filters.command(commands, USERBOT_PREFIXES)
